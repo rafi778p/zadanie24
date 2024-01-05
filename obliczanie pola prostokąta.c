c37a98cacc6590bca0bd177ad8111bdd588864ae
J#### D##### <j-d-pwsz@o2.pl>
2024-01-05 11:56:39 +0100

    Obliczanie pola kola.

--- obliczanie pola prostokąta.c
+++ obliczanie pola prostokąta.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+
+float pole_kola(float r) {
+    return(M_PI*r*r);
+}
+
+void pole_kola_obsluga() {
+    float r, area;
+
+    printf("Wprowadz promien: ");
+    scanf("%f", &r);
+
+    area = pole_kola(r);
+
+    printf("Pole kola = %f\n", pole_kola(r));
+}
 
 float pole_prostokata(float length, float width) {
 	return(length * width);
@@ -14,12 +30,13 @@ void pole_prostokata_obsluga() {
 
     area = pole_prostokata(length, width);
 
-    printf("Pole prostokata = %f", area);	
+    printf("Pole prostokata = %f", area);
 }
 
 int main()
 {
 	pole_prostokata_obsluga();
-	
+	pole_kola_obsluga();
+
     return 0;
 }
